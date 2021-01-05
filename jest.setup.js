78b5440deb41a4a0b2f8ccfc1881a546d2909fb0
lavieennoir <dmitriykhiznyak@gmail.com>
2021-01-05 10:59:26 +0200

    fix test suites

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,14 @@
 const dotenv = require('dotenv');
 const fs = require('fs');
+require('reflect-metadata');
 
-const env = dotenv.parse(fs.readFileSync('.env'));
-const testEnv = dotenv.parse(fs.readFileSync('.env.test'));
+let env = {};
+let testEnv = {};
+try {
+  env = dotenv.parse(fs.readFileSync('.env'));
+} catch {}
+try {
+  testEnv = dotenv.parse(fs.readFileSync('.env.test'));
+} catch {}
 
 Object.assign(process.env, env, testEnv);
